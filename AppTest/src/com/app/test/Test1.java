3ad4a140c2344568e98e6a84a4c052d2d3f485e6
AKG <AKG@192.168.0.17>
2018-11-30 13:46:35 +0530

    add to number

---
+++ AppTest/src/com/app/test/Test1.java
@@ -0,0 +1,11 @@
+package com.app.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=80;
+		int c=a+b;
+		System.out.println("Sum of two number :"+c);
+	}
+}
